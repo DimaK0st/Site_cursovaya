a4e6ac4c49d48ca3602254ee98034e8b9f60cf09
DimaK0st <wwwmistik13@gmail.com>
2020-12-15 23:06:17 +0300

    fix

--- template/interface/header.php
+++ template/interface/header.php
@@ -1,6 +1,7 @@
 <ul id="head_main">
-    <li><a href="/index.php">Головна</a></li>
-    <li><a href="/template/include/selective_generation.php">Масова генерація</a></li>
-    <li><a href="/template/include/sql_geneartor.php">Генерація SQL</a></li>
-    <li><a href="/template/include/about_me.php">Про сайт</a></li>
+
+    <li><a href="/index.php"><h2>Головна</h2></a></li>
+    <li><a href="/template/include/selective_generation.php"><h2>Масова генерація</h2></a></li>
+    <li><a href="/template/include/sql_geneartor.php"><h2>Генерація SQL</h2></a></li>
+    <li><a href="/template/include/about_me.php"><h2>Про сайт</h2></a></li>
 </ul>
